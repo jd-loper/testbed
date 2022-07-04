1c8e06947f2d8aa80470ab01b3c6bdd9b073b5d0
Joseph Loper <106849853+jd-loper@users.noreply.github.com>
2022-07-03 23:14:30 -0700

    Initial commit

---
+++ for-loop.js
@@ -0,0 +1,38 @@
+for (let i = 0; i < 3; i++) {
+    
+    let input = prompt("Enter a number from 1 - 10:");
+
+    if (input > 10) {
+        console.log("That number is too high.");
+    } else {
+        console.log(`You entered: ${input}.`);
+    }
+}
+
+/* A 'for' statement makes a loop that consists of three optional
+expressions, followed by a statement to be executed within the
+loop.
+
+Initialization: An expression or variable that is evaluated
+before the loop begins, such as a counter variable.
+
+Condition: An expression to be evaluated before each iteration
+of the loop. If the expression is 'true' then the statement
+will be executed.
+
+Final-expression: The expression to be evaluated at the end of
+each loop iteration. Used to update or increment a counter
+variable. */
+
+let str = "";
+
+for (i = 1; i < 11; i++) {
+    str = str + i;
+}
+
+console.log(str);
+
+/* The above loop will count up from 1 to 10.
+
+The counter starts at 1, ends at 10, and counts +1 each
+iteration. */
