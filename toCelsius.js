05dabd7da97510241a35130f248e67c73f082096
jd-loper <joedanlop@yahoo.com>
2022-07-08 14:44:43 -0700

    Write program

--- toCelsius.js
+++ toCelsius.js
@@ -4,3 +4,8 @@ Celsius.
 The user will be prompted to input a value in Fahrenheit.
 The program will run the value through a conversion formula.
 The program will output the new Celsius value. */
+
+let userInput = prompt("Enter a value for Fahrenheit:");
+let convert = (userInput - 32) * 5 / 9;
+const c = `${userInput}F = ${convert}C`
+console.log(c);
