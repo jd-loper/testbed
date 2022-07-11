8492c479a7e46e2216510f85c17cf3d23a36806d
Joseph Loper <106849853+jd-loper@users.noreply.github.com>
2022-07-11 01:10:09 -0700

    Update formatting

--- toCelsius.js
+++ toCelsius.js
@@ -1,16 +1,16 @@
 /* This program will convert Fahrenheit values to
-Celsius. */
+Celsius, and Celsius to Fahrenheit. */
 
 const ftoc = function(x) {
-    let fResult = ((x - 32) * 5 / 9);
-    return Math.round(fResult * 10) / 10;
-  };
+  let fResult = ((x - 32) * 5 / 9);
+  return Math.round(fResult * 10) / 10;
+};
   
-  ftoc(-100);
+ftoc(-100);
   
-  const ctof = function(y) {
-    let cResult = (y * 9/5) + 32;
-    return Math.round(cResult * 10) / 10;
-  };
+const ctof = function(y) {
+  let cResult = (y * 9/5) + 32;
+  return Math.round(cResult * 10) / 10;
+};
   
-  ctof(-10);
+ctof(-10);
