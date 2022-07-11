ee402bc551d308d241f2637dae2ee95b2714acc9
jd-loper <joedanlop@yahoo.com>
2022-07-10 20:24:02 -0700

    Update notes

--- stringMethods.js
+++ stringMethods.js
@@ -8,8 +8,7 @@ The 'separator' defines where each split should occur.
 
 The 'limit' is a non-negative integer specifying a limit on
 the number of substrings to be included in the array. Any
-leftover text beyond the limit is not included in the
-array. */
+leftover text is not included in the array. */
 
 const str = "Split this string.";
 const split = str.split(" ");
@@ -23,6 +22,23 @@ together to form a new string.
 This is because strings are immutable and cannot be
 reversed in-place. */
 
-const text = "hello world";
+const text = "Hello World!";
 const reverseText = text.split("").reverse().join("");
 console.log(reverseText);
+
+/* The 'repeat()' method returns a string with a
+number of copies of the string. */
+
+text.repeat(2); // "Hello World!Hello World!"
+
+/* The 'charAt()' method returns a character in the string
+at a specified index position. */
+
+text.charAt(0); // "H"
+
+/* You can concatenate two or more strings with the
+'concat()' method. */
+
+let text1 = "race";
+let text2 = "car";
+let newString = text1.concat(text2);
