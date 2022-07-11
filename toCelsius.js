e3bdd4c8dc7f689c126e4e999b21613f43a9fdbd
jd-loper <joedanlop@yahoo.com>
2022-07-10 23:09:59 -0700

    Convert to functions

--- toCelsius.js
+++ toCelsius.js
@@ -1,11 +1,16 @@
 /* This program will convert Fahrenheit values to
-Celsius.
+Celsius. */
 
-The user will be prompted to input a value in Fahrenheit.
-The program will run the value through a conversion formula.
-The program will output the new Celsius value. */
-
-let userInput = prompt("Enter a value for Fahrenheit:");
-let convert = (userInput - 32) * 5 / 9;
-const c = `${userInput}F = ${convert}C`
-console.log(c);
+const ftoc = function(x) {
+    let fResult = ((x - 32) * 5 / 9);
+    return Math.round(fResult * 10) / 10;
+  };
+  
+  ftoc(-100);
+  
+  const ctof = function(y) {
+    let cResult = (y * 9/5) + 32;
+    return Math.round(cResult * 10) / 10;
+  };
+  
+  ctof(-10);
