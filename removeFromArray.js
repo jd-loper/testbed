9a8b11b72035072da208e5c8644828756194baa1
jd-loper <joedanlop@yahoo.com>
2022-07-15 01:27:29 -0700

    Rewrite with filter method

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,13 +1,6 @@
-const removeFromArray = function(...args) {
-    const array = args[0];
-    const newArray = [];
-    array.forEach(item => {
-        if (!args.includes(item)) {
-            newArray.push(item);
-        }
-    });
-    return newArray;
-};
-
-/* '...args' is a rest parameter. The syntax allows a function to
-accept an indefinite number of arguments as an array. */
+function removeFromArray(...args) {
+    const arr = args[0];
+    return arr.filter((elem) => {
+        return !args.includes(elem);
+    })
+}
